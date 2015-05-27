5aad9099417d09dda833ed4a7a502b63492cccab
luoyepgy <zhanghan@ltbl.cn>
2015-05-27 19:04:20 +0800

    在线活动页面切图

--- js/common.js
+++ js/common.js
@@ -3,9 +3,21 @@ $(function() {
 
 	// 头像效果
     $('.jq-portrait').hover(function() {
-        $(this).next('.user_portrait_card').removeClass('none');
+        $(this).css('z-index', '2').next('div').removeClass('none');
     }, function() {
-        $(this).next('.user_portrait_card').addClass('none');
+        $(this).css('z-index', '1').next('div').addClass('none');
     });
+
+    // 顶部导航交互
+    $(document).scroll(function() {
+    	var top_distance = $(document).scrollTop();
+	    console.log(top_distance);
+	    if(top_distance > 54) {
+	    	$('.jq-top-nav').addClass('fixed_top_nav');
+	    } else {
+	    	$('.jq-top-nav').removeClass('fixed_top_nav');
+	    }
+    });
+    
 
 })
--- js/pages/index.js
+++ js/pages/index.js
@@ -1,38 +1,59 @@
 
 $(function() {
 
+    // 左侧选项卡菜单点击
+    $('.jq-tabContent').find('ul:first').removeClass('none');
+    $('.jq-tabMenu li').click(function() {
+        var index = $(this).index();
+        $('.jq-tabMenu li').removeClass('tab_current');
+        $(this).addClass('tab_current');
+        $(this).closest('.jq-tabContent').find('ul').addClass('none');
+        $(this).closest('.jq-tabContent').find('ul').eq(index).removeClass('none');
+    });
+
+    // 登录验证提示
+    function signin_validate(selector, messages) {
+        $('em.error').remove();
+        selector.parent().append('<em class="error">' + messages + '</em>');
+    }
+ 
     // 登录表单验证
-    $('.jq-signin').validate({
-        ignore: "",
-        rules: {
-            "user_name": {
-                required: true
-            },
-            "user_password": {
-                required: true
-            },
-            "verification_code": {
-                required: true
-            }
-        },
-        messages: {
-            "user_name": {
-                required: "请输入用户名/邮箱！"
-            },
-            "user_password": {
-                required: "请输入密码！"
-            },
-            "verification_code": {
-                required: "请输入验证码！"
-            }
-        },
-        errorPlacement: function(error, element) { 
-            error.appendTo(element.parent('p')); 
-        },
-        errorElement: "em",
-        onkeyup: true,
-        submitHandler: function(form) {  
-            form.submit();
+    $('.jq-signinBtn').click(function() {
+        var user_name = $('.jq-userName').val();
+        var user_password = $('.jq-userPassword').val();
+        var verification_code = $('.jq-code').val();
+        var remember = $('input[name="remember"]').prop('checked');
+        $('em.error').remove();
+        if(user_name == '') {
+            signin_validate($('.jq-userName'), '请输入邮箱/手机号！');
+        } else if(user_password == '') {
+            signin_validate($('.jq-userPassword'), '请输入密码！');
+        } else if(verification_code == '') {
+            signin_validate($('.jq-code'), '请输入验证码！');
+        } else if(user_password.length < 6) {
+            signin_validate($('.jq-userPassword'), '密码至少输入6位！');
+        } else {
+            $.ajax({
+                type: "GET", // 此处应用POST
+                url: "a",
+                dataType: "json",
+                data: {
+                    "user_name": user_name,
+                    "user_password": user_password,
+                    "verification_code": verification_code,
+                    "remember": remember
+                },
+                success: function (data) {
+                    if(data.status == 1) {         
+                        window.location.href = data.url;
+                    } else if(data.status == 0) {
+                        signin_validate($('.jq-code'), data.msg);
+                    }        
+                },
+                error: function() {
+                    signin_validate($('.jq-code'), '加载数据错误！');
+                }
+            });
         }
     });
 
