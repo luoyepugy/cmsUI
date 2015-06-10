6be04d45e1cd620fa81e1dcba505f4af19c48c3e
luoyepgy <zhanghan@ltbl.cn>
2015-06-10 18:14:51 +0800

    圈子成员管理切图

--- js/pages/circle.js
+++ js/pages/circle.js
@@ -1,6 +1,37 @@
 
+(function($) {
+
+	// 成员管理按钮组
+	$.adminMember = function(options) {
+		var admin = {
+			button: '',
+			icon_button: ''
+		};
+
+		options && $.extend(admin, options);
+
+		$(admin.button).hover(function() {
+			$(this).find('.adminBtn').remove();
+			$(this).append($('.adminBtn_wrap').html());
+			$(this).find(admin.icon_button).removeClass('none');
+		}, function() {
+			$(this).find('.adminBtn').remove();
+		});
+	};
+
+})(jQuery);
+
+
 $(function() {
 
+	$.adminMember({
+		button: '.jq-admin',
+		icon_button: '.icon-playback-rewind'
+	});
 
+	$.adminMember({
+		button: '.jq-member',
+		icon_button: '.icon-playback-rewind2'
+	});
 
 });
